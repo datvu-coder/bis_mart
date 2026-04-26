2126189b404cc8fbfb4f3d4d57c14c3bfa4f97ce
Quan Ly Van Ban <user@example.com>
2026-04-26 19:39:44 +0700

    Update web build

--- bismart_flutter/build/web/flutter_bootstrap.js
+++ bismart_flutter/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "57380141"
+    serviceWorkerVersion: "3314366337"
   }
 });
